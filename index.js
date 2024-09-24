df9f1f19cd5061f1623cc01bf6720eb0946c7f83
arshullah <ullaharshullah@gmail.com>
2024-09-24 17:29:31 +0530

    loopjs'

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('Arsh javascript!')
+
+a = 234;
+console.log(a);
+console.log(typeof a);
+
+
+message = "be cool";
+console.log(message);
+console.log(typeof message)
+console.log(String);
+
+
+isHungry = true;
+console.log(isHungry);
+
+
+[x, y, z] = [2, 5, 6];
+console.log(x + y + z);
+
+
+// exponent operator
+
+console.log(2 ** 3);
+console.log(2 + 3);
+console.log(2 + 3 + '4');
+console.log(2 + '3' + 4);
+console.log('2' + 3 + 4);
+console.log(null + 1);
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(1 === '1');
